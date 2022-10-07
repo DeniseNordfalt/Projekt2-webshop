85c5d01f074a3841f0df83254f25a019017cf28f
Denise Nordfalt <denisenordfalt@gmail.com>
2022-10-07 13:33:30 +0200

    corrected string

--- workspaces/server/src/app.ts
+++ workspaces/server/src/app.ts
@@ -2,10 +2,11 @@ import express, { Application, json, Request, Response } from "express";
 import cors from "cors";
 
 const app: Application = express();
+
 app.use(cors());
 app.use(json());
 
-const port: number = parseInt(`process.env.SERVER_PORT || 3001`);
+const port: number = parseInt(process.env.SERVER_PORT || `3001`);
 
 app.get("/hello", (req: Request, res: Response) => {
   res.send("Hello, World!");
