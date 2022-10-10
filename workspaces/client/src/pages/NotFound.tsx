2b4cf1a238a04f2faf33a0ee6468bb72917836c3
Denise Nordfalt <denisenordfalt@gmail.com>
2022-10-10 22:52:04 +0200

    NotFound first commit

--- workspaces/client/src/pages/NotFound.tsx
+++ workspaces/client/src/pages/NotFound.tsx
@@ -1,7 +1,32 @@
 import React from "react";
+import styled from "styled-components";
+
+const StyledDiv = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+  height: 100vh;
+  width: 100vw;
+  background-color: #f5f5f5;
+  color: #333;
+  font-size: 2rem;
+  font-weight: 700;
+`;
+
+const H1 = styled.h1`
+  font-size: 10rem;
+  font-weight: 700;
+  color: red;
+`;
 
 type Props = {};
 
 export default function NotFound({}: Props) {
-  return <div>404</div>;
+  return (
+    <StyledDiv>
+      <H1>404</H1>
+      <p>Page not found</p>
+    </StyledDiv>
+  );
 }
