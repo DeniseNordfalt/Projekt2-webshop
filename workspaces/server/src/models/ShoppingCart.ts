fc358c6f0225973300df53ec479964a1a4e389d0
karl dollmayer <karldollmayer@gmail.com>
2022-10-12 14:15:54 +0200

    update Cart-model

--- workspaces/server/src/models/ShoppingCart.ts
+++ workspaces/server/src/models/ShoppingCart.ts
@@ -6,6 +6,14 @@ import { CartItem, ProductItem } from '@project-webbshop/shared'
 const ShoppingCart = new mongoose.Schema({
     user: { type: String, required: true },
     product: { type: String, required: true },
+    description: { type: String, required: true },
+    category: { type: String, required: true, lowecase: true },
+    weight: { type: String, required: true },
+    price: { type: String, required: true },
+    manufacturer: { type: String, required: true },
+    images: [
+        { type: String, required: true }
+    ],
     paid: { type: Boolean, default: false, required: true }
 
 })
@@ -25,7 +33,7 @@ export const createShoppingCart = async (cartItem: CartItem): Promise<void> => {
     await ShoppingCartModel.create(cartItem)
 }
 export const createPurchase = async (userId: string): Promise<void> => {
-    await ShoppingCartModel.updateMany({ user: userId }, { paid: true })
+    await ShoppingCartModel.updateMany({ user: userId, paid: false }, { paid: true })
 }
 
 export const deleteShoppingCartItem = async (userId: string, product: string): Promise<void> => {
--- workspaces/shared/src/CartItem.ts
+++ workspaces/shared/src/CartItem.ts
@@ -2,5 +2,11 @@ export interface CartItem {
     _id?: string,
     user: string,
     product: string,
+    description: string,
+    category: string,
+    weight: string,
+    price: string,
+    manufacturer: string,
+    images: string[]
     paid: boolean
 }
