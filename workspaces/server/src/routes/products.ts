52b54671239f85a90240f6ecab6d80e63a33f720
Denise Nordfalt <denisenordfalt@gmail.com>
2022-10-20 15:11:51 +0200

    update, delete and get by categories

--- workspaces/server/src/routes/products.ts
+++ workspaces/server/src/routes/products.ts
@@ -2,7 +2,10 @@ import express, { Router } from "express";
 import {
   getProducts,
   getProductById,
+  getProductsByCategory,
   createProduct,
+  updateProduct,
+  deleteProduct,
 } from "../controllers/products";
 
 const router: Router = express.Router();
@@ -10,5 +13,8 @@ const router: Router = express.Router();
 router.get("/", getProducts);
 router.post("/", createProduct);
 router.get("/:id", getProductById);
+router.patch("/:id", updateProduct);
+router.delete("/:id", deleteProduct);
+router.get("/category/:category", getProductsByCategory);
 
 export default router;
