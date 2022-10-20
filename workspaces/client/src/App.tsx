16e9c0013a8444e3f94e1da419d0fe0d1309c8af
Denise Nordfalt <denisenordfalt@gmail.com>
2022-10-20 13:27:52 +0200

    user login and register

--- workspaces/client/src/App.tsx
+++ workspaces/client/src/App.tsx
@@ -1,19 +1,24 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
+import Layout from "./components/Layout";
 
 import "./App.css";
 
 import Home from "./pages/Home";
 import NotFound from "./pages/NotFound";
 import ProductPage from "./pages/ProductPage";
+import UserPage from "./pages/UserPage";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="*" element={<NotFound />} />
-      <Route path="/:id" element={<ProductPage />}/>
-    </Routes>
+    <Layout>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="*" element={<NotFound />} />
+        <Route path="/:id" element={<ProductPage />} />
+        <Route path="/users" element={<UserPage />} />
+      </Routes>
+    </Layout>
   );
 }
 
---
+++ workspaces/client/src/components/UserModal.tsx
@@ -0,0 +1,95 @@
+import React, { useState, useContext } from "react";
+import { loginUser, registerUser } from "../api";
+
+type Props = {};
+
+export default function UserModal({}: Props) {
+  const [name, setname] = useState("");
+  const [email, setemail] = useState("");
+  const [password, setpassword] = useState("");
+  const [adress, setadress] = useState("");
+  const [phone, setphone] = useState("");
+
+  const handleLogin = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const data = await loginUser(email, password);
+    console.log("data", data);
+  };
+
+  const handleRegister = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const data = await registerUser(name, email, password, phone, adress);
+    console.log("data", data);
+  };
+
+  return (
+    <>
+      <div>
+        <h2>usermodal</h2>
+
+        {/* LOGIN FORM*/}
+        <div>
+          <h3>Login</h3>
+          <form onSubmit={handleLogin}>
+            <label>Email</label>
+            <input
+              type="text"
+              name="email"
+              value={email}
+              onChange={(e) => setemail(e.target.value)}
+            />
+            <label>Password</label>
+            <input
+              type="password"
+              name="password"
+              value={password}
+              onChange={(e) => setpassword(e.target.value)}
+            />
+            <button>Login</button>
+          </form>
+        </div>
+
+        {/* REGISTER FORM*/}
+        <div>
+          <h3>Register</h3>
+          <form onSubmit={handleRegister}>
+            <label>Name</label>
+            <input
+              type="text"
+              name="name"
+              onChange={(e) => setname(e.target.value)}
+            />
+            <label>Email</label>
+            <input
+              type="text"
+              name="email"
+              onChange={(e) => setemail(e.target.value)}
+            />
+            <label>Password</label>
+            <input
+              type="password"
+              name="password"
+              onChange={(e) => setpassword(e.target.value)}
+            />
+
+            <label>Phone number</label>
+            <input
+              type="text"
+              name="phonenumber"
+              onChange={(e) => setphone(e.target.value)}
+            />
+
+            <label>Delivery Adress</label>
+            <input
+              type="text"
+              name="adress"
+              onChange={(e) => setadress(e.target.value)}
+            />
+
+            <button>Register</button>
+          </form>
+        </div>
+      </div>
+    </>
+  );
+}
