ac82b002e23a199868f35098faa1fee4df338b17
Denise Nordfalt <denisenordfalt@gmail.com>
2022-10-25 11:38:46 +0200

    style changes

--- workspaces/client/src/components/ProfileCard/styles.tsx
+++ workspaces/client/src/components/ProfileCard/styles.tsx
@@ -36,25 +36,21 @@ export const BodyTitle = styled.h3`
   font-size: 1.2rem;
   font-weight: 600;
   margin: 0;
+  margin-top: 1rem;
 `;
 
-export const ProfileCardBodyText = styled.p`
-  font-size: 1rem;
-  font-weight: 400;
-  margin: 0;
-`;
-
-export const TextItem = styled.p`
-  font-size: 1rem;
-  font-weight: 400;
-  margin: 0;
-  padding: 0.5rem 0;
+export const TextItem = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  margin: 0.5rem 0;
 `;
 
 export const TextItemTitle = styled.p`
   font-size: 1rem;
   font-weight: 600;
   margin: 0;
+  padding-right: 10px;
 `;
 
 export const TextItemValue = styled.p`
@@ -65,7 +61,7 @@ export const TextItemValue = styled.p`
 
 export const Separator = styled.hr`
   border: 1px solid black;
-  margin: 1rem 0;
+  margin: 0.5rem 0;
 `;
 
 export const Button = styled.button`
